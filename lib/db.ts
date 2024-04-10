a51756238dd15845beb381393ffacdddc603cd66
PS <iamkaratedo@gmail.com>
2024-04-10 12:19:59 -0400

    adding cockroach db, and adjust get-item to pull from new db

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/compat/app'
+/*import firebase from 'firebase/compat/app'
 import 'firebase/compat/database'
 
 try {
@@ -16,3 +16,21 @@ try {
 const root = firebase.database().ref('v0')
 
 export default root
+*/
+
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // Ensuring the Prisma instance is reused during hot-reloading
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export default prisma;
